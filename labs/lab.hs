7b333ef2a1add41590e67285e534022e97d876cf
Valentin Oprea <v.oprea.1@student.rug.nl>
2024-11-14 10:37:18 +0100

    nothing done

---
+++ labs/lab.hs
@@ -0,0 +1,46 @@
+nthPalPrime :: Int -> Integer
+nthPalPrime x 
+    | x == 5 = 11
+    | x < 5 = primePalindromes !! (x - 1)
+    | otherwise = primePalindromes !! (x - 2)
+
+palindromicPrimes :: [Integer]
+palindromicPrimes = [d | d <- primes, isPalindrome d]
+
+isPalindrome :: Integer -> Bool
+isPalindrome x = x == reverse x
+    where
+        reverse y = aux 0 y
+        aux w 0 = w
+        aux w y = aux (w * 10 + (mod y 10)) (div y 10)
+
+primes :: [Integer]
+primes = 2:sieve [3,5..]
+    where
+        sieve (p:xs) = p : sieve [x | x <- xs, mod x p /= 0]
+
+
+primePalindromes :: [Integer]
+primePalindromes = filter isPrime oddDigitPalindromes
+
+oddDigitPalindromes :: [Integer]
+oddDigitPalindromes = [makeOddPalindrome n | n <- [1..]]
+
+-- Function to create an odd-length palindrome from a number `n`
+makeOddPalindrome :: Integer -> Integer
+makeOddPalindrome n = n * (10 ^ numDigits (n `div` 10)) + reverseDigits (n `div` 10)
+
+-- Helper function to count the number of digits in a number
+numDigits :: Integer -> Integer
+numDigits 0 = 0
+numDigits x = 1 + numDigits (x `div` 10)
+
+-- Helper function to reverse the digits of a number
+reverseDigits :: Integer -> Integer
+reverseDigits = go 0
+  where
+    go rev 0 = rev
+    go rev x = go (rev * 10 + x `mod` 10) (x `div` 10)
+
+
+isPrime n = n == head (dropWhile (< n) primes)
--- labs/lab1.hs
+++ labs/lab1.hs
@@ -8,3 +8,17 @@ fusc n = f n 0 1
             | n == 2 * k + 1 = f k b (a + b)
             where
                 k = div n 2
+
+
+
+
+
+findPalPrime :: Int -> [Integer] -> Integer
+findPalPrime x w
+    | x <= 0 = 0 
+    | otherwise = aux 0 w
+    where
+        aux k (y:ys)
+            | isPalindrome y && k + 1 == x = y
+            | isPalindrome y = aux (k + 1) ys
+            | otherwise = aux k ys
